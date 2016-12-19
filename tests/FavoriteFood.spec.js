14a7fd3389afe7ccb821edc5e55d665e8acc0ebc
karuna24s <karuna24s@gmail.com>
2016-12-19 08:35:59 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,21 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	it('should filter on food correctly', function () {
+       var list = [{
+           name: 'Karuna',
+           favoriteFood: 'Butter Chicken'
+       }, {
+           name: 'Beenu',
+           favoriteFood: 'Croissant'
+       }, {
+           name: 'Naren',
+           favoriteFood: 'Amritsari Kulcha'
+       }];
+
+			 var results = $filter('favoriteFood')(list, 'Croissant');
+
+        expect(results.length).toBe(1);
+        expect(results[0].name).toBe('Beenu');
+    });
 });
